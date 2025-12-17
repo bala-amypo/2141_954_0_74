dc1002c427fd1275220e269c6be268cba8a402db
bala-amypo <bala@amypo.in>
2025-12-17 09:46:10 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerApiConfig.java
+++ src/main/java/com/example/demo/config/SwaggerApiConfig.java
@@ -1,10 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class SwaggerApiConfig {
@@ -12,9 +11,9 @@ public class SwaggerApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9005.vs.amypo.ai")
-                ));
-        }
+                .info(new Info()
+                        .title("Student Management API")
+                        .version("1.0")
+                        .description("Spring Boot CRUD APIs for Student Management"));
+    }
 }
